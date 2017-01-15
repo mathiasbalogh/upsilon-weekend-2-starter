f8e0ba2899470beef9d8c5c2b37afeade43869fd
Mathias Balogh <mathiasbalogh@Mathias-MacBook-Air.local>
2017-01-15 15:14:12 -0600

    Styling updates Fade inout working and neverending carousel running

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,4 +1,4 @@
-var i =0;
+var i = 0;
 var next = i+1;
 var prev = i-1;
 $(document).ready(function(){
@@ -13,44 +13,81 @@ $(document).ready(function(){
         $('#shoutout').text(person.shoutout);
         $('#'+i).addClass('highlighted')
 
-
         $('#next').on('click', function(){
+          $('#person').fadeOut('slow');
           if(i <16 ){
             $('.highlighted').removeClass('highlighted');
             i = next;
-            var person = response.people[i];
-            $('#name').text(person.name);
-            $('#githubUserName').text(person.githubUserName);
-            $('#shoutout').text(person.shoutout);
+            var timeout = setTimeout(function(){
+              var person = response.people[i];
+              $('#name').text(person.name);
+              $('#githubUserName').text(person.githubUserName);
+              $('#shoutout').text(person.shoutout);
+            },500);
             console.log(i);
             next = i+1;
             prev = i-1;
+            $('#person').fadeIn('slow');
             $(this).closest('body').find('#'+i).addClass('highlighted');
-         }
+          } else {
+            $('.highlighted').removeClass('highlighted');
+            i = 0;
+            next = 1;
+            prev = 16;
+            var timeout = setTimeout(function(){
+              var person = response.people[i];
+              $('#name').text(person.name);
+              $('#githubUserName').text(person.githubUserName);
+              $('#shoutout').text(person.shoutout);
+            },500);
+            $('#person').fadeIn('slow');
+            $(this).closest('body').find('#'+i).addClass('highlighted');
+          }
        });
         $('#prev').on('click', function(){
+          $('#person').fadeOut('slow');
           if(i > 0){
             $('.highlighted').removeClass('highlighted');
             i = prev;
-            var person = response.people[i];
-            $('#name').text(person.name);
-            $('#githubUserName').text(person.githubUserName);
-            $('#shoutout').text(person.shoutout);
+            var timeout = setTimeout(function(){
+              var person = response.people[i];
+              $('#name').text(person.name);
+              $('#githubUserName').text(person.githubUserName);
+              $('#shoutout').text(person.shoutout);
+            },500);
+            $('#person').fadeIn('slow');
             console.log(i);
            next = i+1;
            prev = i-1;
            $(this).closest('body').find('#'+i).addClass('highlighted');
+         } else {
+           $('.highlighted').removeClass('highlighted');
+           i = 16;
+           next = 0;
+           prev = 15;
+           var timeout = setTimeout(function(){
+             var person = response.people[i];
+             $('#name').text(person.name);
+             $('#githubUserName').text(person.githubUserName);
+             $('#shoutout').text(person.shoutout);
+           },500);
+           $('#person').fadeIn('slow');
+           $(this).closest('body').find('#'+i).addClass('highlighted');
          }
        });
        $('.carousel').on('click', function(){
+         $('#person').fadeOut('slow');
          $('.highlighted').removeClass('highlighted');
          i = Number($(this).attr('id'));
          next = i+1;
          prev = i-1;
-         var person = response.people[i];
-         $('#name').text(person.name);
-         $('#githubUserName').text(person.githubUserName);
-         $('#shoutout').text(person.shoutout);
+         var timeout = setTimeout(function(){
+           var person = response.people[i];
+           $('#name').text(person.name);
+           $('#githubUserName').text(person.githubUserName);
+           $('#shoutout').text(person.shoutout);
+         },500);
+         $('#person').fadeIn('slow');
          $(this).addClass('highlighted');
        });
     }
