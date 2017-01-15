304883a74e9bcdb33438714dc304872c94d36d4b
Mathias Balogh <mathiasbalogh@Mathias-MacBook-Air.local>
2017-01-15 16:37:35 -0600

    Promode complete

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,6 +1,7 @@
 var i = 0;
 var next = i+1;
 var prev = i-1;
+var time = 9;
 $(document).ready(function(){
 
     $.ajax({
@@ -29,6 +30,7 @@ $(document).ready(function(){
             prev = i-1;
             $('#person').fadeIn('slow');
             $(this).closest('body').find('#'+i).addClass('highlighted');
+            time = 9;
           } else {
             $('.highlighted').removeClass('highlighted');
             i = 0;
@@ -42,6 +44,7 @@ $(document).ready(function(){
             },500);
             $('#person').fadeIn('slow');
             $(this).closest('body').find('#'+i).addClass('highlighted');
+            time= 9;
           }
        });
         $('#prev').on('click', function(){
@@ -60,6 +63,7 @@ $(document).ready(function(){
            next = i+1;
            prev = i-1;
            $(this).closest('body').find('#'+i).addClass('highlighted');
+           time = 9;
          } else {
            $('.highlighted').removeClass('highlighted');
            i = 16;
@@ -73,6 +77,7 @@ $(document).ready(function(){
            },500);
            $('#person').fadeIn('slow');
            $(this).closest('body').find('#'+i).addClass('highlighted');
+           time = 9;
          }
        });
        $('.carousel').on('click', function(){
@@ -89,7 +94,44 @@ $(document).ready(function(){
          },500);
          $('#person').fadeIn('slow');
          $(this).addClass('highlighted');
+         time = 9;
        });
+       var standardInterval = setInterval(function(){
+         time--;
+         if(time == 0){
+         $('#person').fadeOut('slow');
+         if(i <16 ){
+           $('.highlighted').removeClass('highlighted');
+           i = next;
+           var timeout = setTimeout(function(){
+             var person = response.people[i];
+             $('#name').text(person.name);
+             $('#githubUserName').text(person.githubUserName);
+             $('#shoutout').text(person.shoutout);
+           },500);
+           console.log(i);
+           next = i+1;
+           prev = i-1;
+           $('#person').fadeIn('slow');
+           $('#person').closest('body').find('#'+i).addClass('highlighted');
+           time = 9;
+         } else {
+           $('.highlighted').removeClass('highlighted');
+           i = 0;
+           next = 1;
+           prev = 16;
+           var timeout = setTimeout(function(){
+             var person = response.people[i];
+             $('#name').text(person.name);
+             $('#githubUserName').text(person.githubUserName);
+             $('#shoutout').text(person.shoutout);
+           },500);
+           $('#person').fadeIn('slow');
+           $('#person').closest('body').find('#'+i).addClass('highlighted');
+           time = 9;
+         }
+       }
+       }, 1000);
     }
   });
 });
